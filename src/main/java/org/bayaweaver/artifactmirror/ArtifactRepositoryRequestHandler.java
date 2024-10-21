70fd15cd6178043438d91a4013656c656d88928f
Serhii Yatskovskyi <serhii.yatskovskyi@bayaweaver.org>
2024-10-21 16:05:32 -0600

    Fix npm install requests problem

--- src/main/java/org/bayaweaver/artifactmirror/ArtifactRepositoryRequestHandler.java
+++ src/main/java/org/bayaweaver/artifactmirror/ArtifactRepositoryRequestHandler.java
@@ -5,11 +5,18 @@ import com.sun.net.httpserver.HttpHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.InflaterInputStream;
 
 class ArtifactRepositoryRequestHandler implements HttpHandler {
     private static final Logger logger = LoggerFactory.getLogger(ArtifactRepositoryRequestHandler.class);
@@ -74,10 +81,27 @@ class ArtifactRepositoryRequestHandler implements HttpHandler {
     }
 
     private void copyResponse(HttpURLConnection artifactConnection, HttpExchange exchange) throws IOException {
+        for (Map.Entry<String, List<String>> header : artifactConnection.getHeaderFields().entrySet()) {
+            String headerName = header.getKey();
+            if (headerName != null
+                    && !headerName.toLowerCase().startsWith("x-amzn-")
+                    && !headerName.equalsIgnoreCase("Content-Length")) {
+
+                for (String headerValue : header.getValue()) {
+                    exchange.getResponseHeaders().add(headerName, headerValue);
+                }
+            }
+        }
         InputStream artifactResponseBody;
         artifactResponseBody = artifactConnection.getErrorStream();
         if (artifactResponseBody == null) {
             artifactResponseBody = artifactConnection.getInputStream();
+            final String encoding = artifactConnection.getContentEncoding();
+            if ("GZIP".equalsIgnoreCase(encoding)) {
+                artifactResponseBody = new GZIPInputStream(artifactResponseBody);
+            } else if ("DEFLATE".equalsIgnoreCase(encoding)) {
+                artifactResponseBody = new InflaterInputStream(artifactResponseBody);
+            }
         }
         try {
             artifactResponseBody.transferTo(exchange.getResponseBody());
